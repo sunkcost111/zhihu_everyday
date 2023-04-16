4119d64c3ea17af689adf69c1ed9f840ba4c1e65
zhaosai <1162838487.com>
2023-04-16 18:37:30 +0800

    rem完成

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import 'lib-flexible'
 import './index.less'
 import './assets/reset.min.css'
+
+(function () {
+  const handleMax = function handleMax() {
+    let html = document.documentElement
+    let root = document.getElementById('root')
+    let size = parseFloat(html.style.fontSize)
+    let deviceW = html.clientWidth
+    root.style.maxWidth = '750px'
+    if (size >= 75) {
+      html.style.fontSize = '75px'
+    }
+  }
+  handleMax()
+  window.addEventListener('resize', handleMax)
+})()
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>

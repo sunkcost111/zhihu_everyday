daa5589a88c9009853abcf21ca3999440e47dfde
zhaosai <zhaosai02@meituan.com>
2023-05-05 16:02:20 +0800

    测试提交

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Login() {
   return (
     <div className="login-box">
-      登录页面
+      登录页面这是一个测试
     </div>
   )
 }
--- src/views/Page404.jsx
+++ src/views/Page404.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Page404() {
   return (
     <div className="page-404">
-      Page404
+      Page404我们的测试404文件页面
     </div>
   )
 }
